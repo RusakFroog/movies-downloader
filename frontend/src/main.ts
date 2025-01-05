d981e179edc07ef321185db3c3bc8d20a21b40c0
Rusak21 <sg970465@gmail.com>
2025-01-05 01:08:52 +0100

    add stores (pinia)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 import router from './routes';
 import i18n from '../i18n/translate';
 import './utils/index';
 
+const pinia = createPinia();
 const app = i18n(createApp(App));
 
+app.use(pinia);
 app.use(router);
 app.mount('#app');
